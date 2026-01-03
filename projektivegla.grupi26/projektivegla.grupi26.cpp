528f7ca22855c96ab6c46258c1b3b6ebeb671949
elvisberisha1 <elvis.berisha1@student.uni-pr.edu>
2026-01-03 11:15:09 -0800

    Update projektivegla.grupi26.cpp

--- projektivegla.grupi26/projektivegla.grupi26.cpp
+++ projektivegla.grupi26/projektivegla.grupi26.cpp
@@ -15,17 +15,6 @@ int modulo(int a ,int b){
 return a % b ;
 
 }
-int multiply(int a, int b){
-    return a*b;
-}
-
-float divide(int a, int b){
-    if(b==0){
-        cout << "Error: Division by zero!" << endl;
-        return 0;
-    }
-    return  (float)a/b;
-}
 
 int main()
 {
